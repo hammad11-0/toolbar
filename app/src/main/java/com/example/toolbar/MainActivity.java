bab0ccd94f4a13b7dea3ba3651944c844f405209
hammad11-0 <bitf18a504@pucit.edu.pk>
2021-12-05 22:51:19 +0500

    2

--- app/src/main/java/com/example/toolbar/MainActivity.java
+++ app/src/main/java/com/example/toolbar/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar = findViewById(R.id.toolbar);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSupportActionBar(toolbar);
     }
 }
